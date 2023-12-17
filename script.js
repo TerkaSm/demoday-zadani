9df0f1aa98327ca46a2c6274479c603fd1f34d21
Tereza Smolova <tereza.smolova@gmail.com>
2023-12-17 23:30:45 +0100

    Working on js

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 console.log('js');
+
 const productCards = document.querySelector('.products__grid');
+const loadMoreButton = document.querySelector('#load-more-button');
 
-const jsonUrl = 'assets/src/products.json';
+let displayedProducts = 4;
 
-// Fetch request pro získání dat z JSON souboru
-fetch(jsonUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    // Zpracování dat a vytvoření obsahu pro prodejní kartu
-    const cardContent = `
-        <div class="product-card">
-          <img class="product-card__img" src="${data.imgSrc}" alt="${
-      data.title
-    }">
-          <h3 class="heading-tertiary product-card__title">${data.title}</h3>
-          <p class="product-card__availability product-card__availability--yes">${
-            data.availability
-          }</p>
-          <p class="product-card__price">${data.price} CZK</p>
-          <div class="product-card__labels">
-            // <p class="product-card__label product-card__label--new">Novinka</p>
-            // <p class="product-card__label product-card__label--sale">Výprodej</p>
-            ${
-              Array.isArray(data.flags)
-                ? data.flags
-                    .map((flag) => `<p class="product-card__label">${flag}</p>`)
-                    .join('')
-                : ''
-            }
-          </div>
-          <div class="product-card__button-wrapp"><button class="button button--cart"><img src="assets/icons/cart.svg" alt="nákupní košík"></button></div>
+const response = await fetch('assets/src/products.json', {
+  method: 'GET'
+});
+
+const products = await response.json();
+console.log(products);
+
+function displayProducts(start, end) {
+  for (let i = start; i < end; i++) {
+    const product = products[i];
+    productCards.innerHTML += `
+      <div class="product-card">
+        <img class="product-card__img" src="${product.imgSrc}" alt="${product.title}">
+        <h3 class="heading-tertiary product-card__title">${product.title}</h3>
+        <p class="product-card__availability product-card__availability--yes">${product.availability}</p>
+        <p class="product-card__price">${product.price} CZK</p>
+        <div class="product-card__labels">
+          ${product.flags.map((flag) => `<p class="product-card__label product-card__label--new">${flag}</p>`)}
         </div>
-        `;
+        <div class="product-card__button-wrapp"><button class="button button--cart"><img src="assets/icons/cart.svg" alt="nákupní košík"></button></div>
+      </div>
+    `;
+  }
+}
 
-    productCards.innerHTML = cardContent;
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-    productCards.innerHTML = 'Error loading data';
-  });
+displayProducts(0, displayedProducts);
+
+loadMoreButton.addEventListener('click', () => {
+  displayedProducts += 8;
+  // kontrola jestli ještě jsou produkty k zobrazení
+  if (displayedProducts <= products.length) {
+    // Zobrazit dalších 8 produktů
+    displayProducts(displayedProducts - 8, displayedProducts);
+  } else {
+    loadMoreButton.style.display = 'none';
+  }
+});
+
