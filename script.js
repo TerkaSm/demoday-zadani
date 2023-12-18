722cb176e8e480d5f088387b85f9ece25c7c24fd
Tereza Smolova <tereza.smolova@gmail.com>
2023-12-18 11:29:09 +0100

    Added filter of products

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 console.log('js');
 
-const productCards = document.querySelector('.products__grid');
+let productCards = document.querySelector('.products__grid');
 const loadMoreButton = document.querySelector('#load-more-button');
+const newsButton = document.querySelector('#news-button');
+const bestsellerButton = document.querySelector('#bestseller-button');
+const recommendedButton = document.querySelector('#recommended-button');
 
 let displayedProducts = 4;
 
@@ -10,7 +13,6 @@ const response = await fetch('assets/src/products.json', {
 });
 
 const products = await response.json();
-console.log(products);
 
 function displayProducts(start, end) {
   for (let i = start; i < end; i++) {
@@ -53,4 +55,115 @@ loadMoreButton.addEventListener('click', () => {
     loadMoreButton.style.display = 'none';
   }
 });
+
+
+// Filtr produktů podle flag "Výprodej"
+
+bestsellerButton.addEventListener('click', () => {
+  //location.reload();
+  const bestsellersProducts = products.filter((product) => product.flags.includes('Výprodej'));
+  console.log(bestsellersProducts)
+
+  productCards.innerHTML = ''; // Vyčistit seznam produktů
+  
+  bestsellersProducts.forEach((product) => {
+    productCards.innerHTML += `
+      <div class="product-card">
+        <img class="product-card__img" src="${product.imgSrc}" alt="${product.title}">
+        <h3 class="heading-tertiary product-card__title">${product.title}</h3>
+        <p class="product-card__availability 
+          ${product.availability === 'Momentálně nedostupné' ? 'product-card__availability--no' : ''}
+          ${product.availability === 'Skladem' ? 'product-card__availability--yes' : ''}
+          ${product.availability === 'Na objednávku' ? 'product-card__availability--on-order' : ''}">
+          ${product.availability}
+        </p>
+        <p class="product-card__price">${product.price} CZK</p>
+        <div class="product-card__labels">
+        ${product.flags.map((flag) => `
+        <p class="product-card__label 
+        ${flag === 'Novinka' ? 'product-card__label--new' : ''}
+        ${flag === 'Tip' ? 'product-card__label--tip' : ''}
+        ${flag === 'Výprodej' ? 'product-card__label--sale' : ''} ">
+        ${flag}
+        </p>`)}
+        </div>
+        <div class="product-card__button-wrapp"><button class="button button--cart"><img src="assets/icons/cart.svg" alt="nákupní košík"></button></div>
+      </div>
+    `;
+  });
+})
+
+
+// Filtr produktů podle flag "Novinka"
+
+newsButton.addEventListener('click', () => {
+  //location.reload();
+  const newsProducts = products.filter((product) => product.flags.includes('Novinka'));
+  console.log(newsProducts)
+
+  productCards.innerHTML = ''; // Vyčistit seznam produktů
+  
+  newsProducts.forEach((product) => {
+    productCards.innerHTML += `
+      <div class="product-card">
+        <img class="product-card__img" src="${product.imgSrc}" alt="${product.title}">
+        <h3 class="heading-tertiary product-card__title">${product.title}</h3>
+        <p class="product-card__availability 
+          ${product.availability === 'Momentálně nedostupné' ? 'product-card__availability--no' : ''}
+          ${product.availability === 'Skladem' ? 'product-card__availability--yes' : ''}
+          ${product.availability === 'Na objednávku' ? 'product-card__availability--on-order' : ''}">
+          ${product.availability}
+        </p>
+        <p class="product-card__price">${product.price} CZK</p>
+        <div class="product-card__labels">
+        ${product.flags.map((flag) => `
+        <p class="product-card__label 
+        ${flag === 'Novinka' ? 'product-card__label--new' : ''}
+        ${flag === 'Tip' ? 'product-card__label--tip' : ''}
+        ${flag === 'Výprodej' ? 'product-card__label--sale' : ''} ">
+        ${flag}
+        </p>`)}
+        </div>
+        <div class="product-card__button-wrapp"><button class="button button--cart"><img src="assets/icons/cart.svg" alt="nákupní košík"></button></div>
+      </div>
+    `;
+  });
+})
+
+
+// Filtr produktů podle flag "Tip"
+
+recommendedButton.addEventListener('click', () => {
+  //location.reload();
+  const recommendedProducts = products.filter((product) => product.flags.includes('Tip'));
+  console.log(recommendedProducts)
+
+  productCards.innerHTML = ''; // Vyčistit seznam produktů
+  
+  recommendedProducts.forEach((product) => {
+    productCards.innerHTML += `
+      <div class="product-card">
+        <img class="product-card__img" src="${product.imgSrc}" alt="${product.title}">
+        <h3 class="heading-tertiary product-card__title">${product.title}</h3>
+        <p class="product-card__availability 
+          ${product.availability === 'Momentálně nedostupné' ? 'product-card__availability--no' : ''}
+          ${product.availability === 'Skladem' ? 'product-card__availability--yes' : ''}
+          ${product.availability === 'Na objednávku' ? 'product-card__availability--on-order' : ''}">
+          ${product.availability}
+        </p>
+        <p class="product-card__price">${product.price} CZK</p>
+        <div class="product-card__labels">
+        ${product.flags.map((flag) => `
+        <p class="product-card__label 
+        ${flag === 'Novinka' ? 'product-card__label--new' : ''}
+        ${flag === 'Tip' ? 'product-card__label--tip' : ''}
+        ${flag === 'Výprodej' ? 'product-card__label--sale' : ''} ">
+        ${flag}
+        </p>`)}
+        </div>
+        <div class="product-card__button-wrapp"><button class="button button--cart"><img src="assets/icons/cart.svg" alt="nákupní košík"></button></div>
+      </div>
+    `;
+  });
+})
 
